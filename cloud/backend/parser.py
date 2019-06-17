2c7ab31aa670282cd1711af183afbcd9ee1ffef3
AstraLee <astra_1995@hotmail.com>
2019-06-17 15:22:22 +0800

    send message to web

--- cloud/backend/parser.py
+++ cloud/backend/parser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import json
 import asyncio
+import socket
 import websockets
 
 with open("data.txt", "r") as f:
@@ -14,10 +15,32 @@ async def sendMessage(websocket, path):
     await websocket.send(payload)
     
 def main():
-    start_server = websockets.serve(sendMessage, '127.0.0.1', 8081)
+    start_server = websockets.serve(sendMessage, '140.112.14.145', 8081)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
     
 
 if __name__ == "__main__":
     main()
+
+
+"""
+async def sendMessage(websocket, path):
+    await websocket.send(payload)
+    
+def main():
+    start_server = websockets.serve(sendMessage, '140.112.14.145', 8081)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
+
+if __name__ == "__main__":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(('127.0.0.1', 8888))
+        data = s.recv(1024)
+        print('Received', repr(data))
+        payload = json.dumps(data)
+
+    main()
+
+"""
